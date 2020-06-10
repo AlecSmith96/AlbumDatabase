7d03a2b6a95249568c5f78a2e9cf07469e956310
Alec <rcsmith.alec@gmail.com>
2020-06-10 20:09:58 +0100

    implemented Track object

--- Track/Track.cpp
+++ Track/Track.cpp
@@ -1,5 +1,37 @@
-//
-// Created by Lovel on 10/06/2020.
-//
-
+/**
+ * Track.cpp
+ * Source file for class describing a Track object.
+ * Created on 10/06/2020
+ */
 #include "Track.h"
+#include "../Duration/Duration.h"
+
+Track::Track() {
+
+}
+
+Track::Track(string title, Duration duration) {
+    this->title = title;
+    this->duration = duration;
+}
+
+string Track::getTitle() {
+    return title;
+}
+
+void Track::setTitle(const string &title) {
+    Track::title = title;
+}
+
+Duration Track::getDuration() {
+    return duration;
+}
+
+void Track::setDuration(const Duration &duration) {
+    Track::duration = duration;
+}
+
+ostream& operator<<(ostream& str, Track &track) {
+    Duration dur = track.getDuration();
+    return str << track.getTitle() << " - " << dur;
+}
--- Track/Track.h
+++ Track/Track.h
@@ -1,14 +1,28 @@
-//
-// Created by Lovel on 10/06/2020.
-//
+/**
+ * Track.h
+ * Header file for class describing a Track object.
+ * Created on 10/06/2020
+ */
+#include <string>
+#include "../Duration/Duration.h"
 
 #ifndef ALBUMDATABASE_TRACK_H
 #define ALBUMDATABASE_TRACK_H
 
-
 class Track {
+private:
+    string title;
+    Duration duration;
 
+public:
+    Track();
+    Track(string title, Duration duration);
+
+    string getTitle();
+    void setTitle(const string &title);
+    Duration getDuration();
+    void setDuration(const Duration &duration);
 };
 
-
+ostream& operator<<(ostream& str, Track &track);
 #endif //ALBUMDATABASE_TRACK_H
